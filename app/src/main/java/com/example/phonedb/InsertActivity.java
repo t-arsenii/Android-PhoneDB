c9e11c102b8ef4ec66008201341ed348b1cf94e2
Arsenii <senyazp80@gmail.com>
2023-05-10 15:49:14 +0200

    new Insert layout

---
+++ app/src/main/java/com/example/phonedb/InsertActivity.java
@@ -0,0 +1,55 @@
+package com.example.phonedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class InsertActivity extends AppCompatActivity {
+
+    Button btn_website;
+    Button btn_cancel;
+    Button btn_save;
+    EditText et_manufactor;
+    EditText et_model;
+    EditText et_andrver;
+    EditText et_website;
+    String RESULT_MANUFACTOR = "";
+    String RESULT_MODEL = "";
+    String RESULT_ANDRVER = "";
+    String RESULT_WEBSITE = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        btn_website = findViewById(R.id.button_WebSite);
+        btn_cancel = findViewById(R.id.button_Cancel);
+        btn_save = findViewById(R.id.button_Save);
+        et_manufactor = findViewById(R.id.EditText_Manufactor);
+        et_model = findViewById(R.id.EditText_Model);
+        et_andrver = findViewById(R.id.EditText_Model);
+        et_website = findViewById(R.id.EditText_WebSite);
+
+        btn_cancel.setOnClickListener(v -> {
+            returnToMain();
+        });
+        btn_save.setOnClickListener(v -> {
+            returnToMainSave();
+        });
+    }
+    private void returnToMain() {
+        finish();
+    }
+    private void returnToMainSave() {
+        Intent intent = new Intent();
+        intent.putExtra(RESULT_MANUFACTOR, et_manufactor.getText().toString());
+        intent.putExtra(RESULT_MODEL, et_model.getText().toString());
+        intent.putExtra(RESULT_ANDRVER, et_andrver.getText().toString());
+        intent.putExtra(RESULT_WEBSITE, et_website.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+}
--- app/src/main/java/com/example/phonedb/MainActivity.java
+++ app/src/main/java/com/example/phonedb/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.phonedb;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
     private PhoneViewModel mPhoneViewModel;
     private PhoneListAdapter mAdapter;
-
+    private FloatingActionButton fab_;
+    private ActivityResultLauncher<Intent> mActivityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab_ = findViewById(R.id.fabMain);
+        fab_.setOnClickListener(v -> {
+            startSecondActivity();
+        });
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         mAdapter = new PhoneListAdapter(this);
         recyclerView.setAdapter(mAdapter);
@@ -25,5 +34,9 @@ public class MainActivity extends AppCompatActivity {
                     mAdapter.setPhoneList(elements);
                 });
 
+    }
+    private void startSecondActivity() {
+        Intent intent = new Intent(this, InsertActivity.class);
+        mActivityResultLauncher.launch(intent);
     }
 }
